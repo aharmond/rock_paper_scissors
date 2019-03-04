266baa89c608d9abf654e7764241cde15a7ba61d
AJ Harmond <ajharmond@gmail.com>
2019-03-04 16:11:34 -0700

    Scorekeeping

--- index.js
+++ index.js
@@ -4,48 +4,63 @@ var playerChoice = [];
 var opponentChoice = [];
 
 var result = document.getElementById("result");
-var pchoice = document.getElementById("pchoice");
-var ochoice = document.getElementById("ochoice");
+var pChoice = document.getElementById("pchoice");
+var oChoice = document.getElementById("ochoice");
+
+var wins = 0
+var losses = 0
+var ties = 0
+var gameResult = ""
+
+var scoreWin = document.getElementById("wins");
+var scoreLoss = document.getElementById("losses");
+var scoreTie = document.getElementById("ties");
 
 var choiceOne = document.getElementById("choice-one");
 var choiceTwo = document.getElementById("choice-two");
 var choiceThree = document.getElementById("choice-three");
 
 choiceOne.addEventListener("click", function() {
-  makeChoice(choiceOne);
+  playerChoice = 0
+  makeChoice(playerChoice);
   playGame(playerChoice);
+  score(gameResult);
 });
 choiceTwo.addEventListener("click", function() {
-  makeChoice(choiceTwo);
+  playerChoice = 1
+  makeChoice(playerChoice);
   playGame(playerChoice);
+  score(gameResult);
 });
 choiceThree.addEventListener("click", function() {
-  makeChoice(choiceThree);
+  playerChoice = 2
+  makeChoice(playerChoice);
   playGame(playerChoice);
+  score(gameResult);
 });
 
 function makeChoice(choice) {
-  playerChoice = choices.filter( function(choices){
-    return choices === choice.innerHTML
-  })
-  playerChoice = playerChoice[0]
+  return playerChoice = choices[choice]
 };
 
 function playGame(choice) {
   opponentChoice = choices[Math.floor(Math.random()*choices.length)]
-  pchoice.innerHTML = "Your choice: " + playerChoice;
-  ochoice.innerHTML = "Opponent choice: " + opponentChoice;
+  pChoice.innerHTML = "Your choice: " + playerChoice;
+  oChoice.innerHTML = "Opponent choice: " + opponentChoice;
   switch(playerChoice) {
     case "Rock":
       switch (opponentChoice) {
         case "Rock":
           result.innerHTML = "Tie";
+          gameResult = "Tie";
           break;
         case "Paper":
           result.innerHTML = "Loss";
+          gameResult = "Loss";
           break;
         case "Scissors":
           result.innerHTML = "Win!";
+          gameResult = "Win";
           break;
       };
       break;
@@ -53,12 +68,15 @@ function playGame(choice) {
       switch (opponentChoice) {
         case "Rock":
           result.innerHTML = "Win!";
+          gameResult = "Win";
           break;
         case "Paper":
           result.innerHTML = "Tie";
+          gameResult = "Tie";
           break;
         case "Scissors":
           result.innerHTML = "Loss";
+          gameResult = "Loss";
           break;
       };
       break;
@@ -66,14 +84,37 @@ function playGame(choice) {
       switch (opponentChoice) {
         case "Rock":
           result.innerHTML = "Loss";
+          gameResult = "Loss";
           break;
         case "Paper":
           result.innerHTML = "Win!";
+          gameResult = "Win";
           break;
         case "Scissors":
           result.innerHTML = "Tie";
+          gameResult = "Tie";
           break;
       };
       break;
   };
+}
+
+function score(gameResult) {
+  scoreWin.innerHTML = "Wins: " + wins;
+  scoreLoss.innerHTML = "Losses: " + losses;
+  scoreTie.innerHTML = "Ties: " + ties;
+  switch (gameResult) {
+    case "Win":
+      wins += 1;
+      scoreWin.innerHTML = "Wins: " + wins;
+      break;
+    case "Loss":
+      losses += 1;
+      scoreLoss.innerHTML = "Losses: " + losses;
+      break;
+    case "Tie":
+      ties += 1;
+      scoreTie.innerHTML = "Ties: " + ties;
+      break;
+  }
 }
